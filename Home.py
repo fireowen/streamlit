d85fb7886d6ce26d4ec9b7c5d7731a1a3ca9fd17
fireowen <owen@safyreconsulting.com.au>
2023-08-23 15:49:42 +1000

    removed logo

--- Home.py
+++ Home.py
@@ -1,11 +1,10 @@
 import streamlit as st
-from streamlit_extras.app_logo import add_logo
 from st_keyup import st_keyup
 from streamlit_extras.switch_page_button import switch_page
 
 import plotly
 
-add_logo(r'images\logo.jpg', height=120)
+
 
 st.title('Safyre calculator homepage')
 
